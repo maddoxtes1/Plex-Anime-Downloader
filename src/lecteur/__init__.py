14ebd7568b438a5b4a433e24adb2a6eb8d526c32
maddox <maddoxtes@gmail.com>
2025-03-20 02:53:11 -0400

    removing the old download system for the new one

--- src/lecteur/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.lecteur.sendvid import sendvid_downloader
-from src.lecteur.sibnet import sibnet_downloader
-from src.lecteur.vidmoly import vidmoly_downloader
-from src.lecteur.oneupload import oneupload_downloader
