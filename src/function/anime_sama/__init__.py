f97c228cc0325a9cd16f3112b74cc2b619b14c69
maddox <maddoxtes@gmail.com>
2025-03-27 20:43:12 -0400

    delete old sctucture

--- src/function/anime_sama/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.function.anime_sama.build_url import build_url
-from src.function.anime_sama.find_episode import find_episode
-from src.function.anime_sama.extract_link import extract_link
-from src.function.anime_sama.compare_json import compare_json
--- src/function/misc/create_path.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class create_path:
-    def __init__(self, path):
-        if not os.path.exists(path):
-            os.makedirs(path)
